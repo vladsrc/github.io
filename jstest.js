acd92f918d3fe631a3303971f4930a5d416b40c4
vladsrc <54189120+vladsrc@users.noreply.github.com>
2021-10-08 12:31:42 +0300

    Add files via upload

---
+++ jstest.js
@@ -0,0 +1,37 @@
+import sqlite3 from 'sqlite3';
+//const sqlite3 = require('sqlite3').verbose();
+
+// open the database
+let db = new sqlite3.Database('./vkdb.db', sqlite3.OPEN_READWRITE, (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Connected to the chinook database.');
+});
+
+db.serialize(() => {
+  db.each(`SELECT LINK as link, ROWW as roww FROM vkph 
+ORDER BY RANDOM() LIMIT 1`, (err, row) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log(row.roww + " | " + row.link);
+  });
+});
+
+db.close((err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Closed the database connection.');
+});
+
+function Start(){
+	db.each(`SELECT LINK as link, ROWW as roww FROM vkph 
+ORDER BY RANDOM() LIMIT 1`, (err, row) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log(row.roww + " | " + row.link);
+  });
+}
